d7f0ba2370899aef7368a87b05e4fa345c393368
dperuo <derek@derekperuo.net>
2020-04-29 18:00:26 -0500

    feat: 🎸 (server-worker) Cache resources

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,29 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
+const logger = {
+  error: (msg) => console.error('==> [ERROR]', msg)
+}
+
+try {
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 Days
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources',
+    })
+  );
+} catch (err) {
+  logger.error(err)
 }
