f64a8283bf52dad05f2416620f787914235fab6d
dperuo <derek@derekperuo.net>
2022-04-29 21:59:13 -0500

    content: (work-with-us.js) Update link

--- work-with-us.js
+++ work-with-us.js
@@ -2,9 +2,10 @@ addEventListener("message", () => console.log(`
 
 Hi there! 👋
 
-Like to look under the hood? Enjoy solving complex code challenges? If yes, we should talk!
+Like looking under the hood? If yes, we should talk!
 
-<LINK HERE>
+Join our team as a full-time Ionic developer and solve complex challenges like data encryption, Bluetooth hardware support, and multi-market rollouts.
 
+Text JOIN to 646-820-8246 to find out more.
 
 `));
